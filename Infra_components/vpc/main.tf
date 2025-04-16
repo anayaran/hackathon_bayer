127c48c37b1d327203beafcddca3642cae23b070
aishwaryap14 <aishwarya.potdar123@gmail.com>
2025-04-16 13:39:26 +0530

    terraform files

--- Infra_components/vpc/main.tf
+++ Infra_components/vpc/main.tf
@@ -5,16 +5,14 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  name   = "ex-${basename(path.cwd)}"
-  region = "eu-west-1"
+  name   = "example_vpc"
+  region = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
     Example    = local.name
-    GithubRepo = "terraform-aws-vpc"
-    GithubOrg  = "terraform-aws-modules"
   }
 }
 
